3f3fdff98755895546cfba985d0de29f1d85720a
Deivisson <deivisson.tecnico@gmail.com>
2024-11-13 12:14:35 -0300

    fix: corrige ignore

--- myfinance-web-dotnet/myfinance-web-dotnet/obj/Debug/net8.0/myfinance-web-dotnet.AssemblyInfo.cs
+++ myfinance-web-dotnet/myfinance-web-dotnet/obj/Debug/net8.0/myfinance-web-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfinance-web-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32c5afb28e6b4aeeeb6372b743bd89a6feae7c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+780157fe40451d135fc216dddf92df4360950042")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfinance-web-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfinance-web-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
